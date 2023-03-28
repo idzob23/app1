656d518c390783ee7141a7b9e3bd9c0b5643ae19
Ilija Bokan <ilija.bokan@voli.me>
2023-03-28 19:43:30 +0200

    First stage in basic GUI without interactions

--- gui.py
+++ gui.py
@@ -5,7 +5,9 @@ label = sg.Text("Type in a to-do")
 input_text = sg.InputText(tooltip="Enter to-do")
 add_button  = sg.Button("Add")
 
+
 window = sg.Window('My To-Do App', layout=[[label], [input_text, add_button]])   #putting object in [] alone will display object on separate lines
 window.read()       #display windows
+
 
 window.close()      #close the windows when press some button
